f5c03dd063f6049400a43facde2ed04bea6f9a85
Marcel Garus <marcel.garus@gmail.com>
2020-10-23 23:30:48 +0200

    Fix exports

--- lib/glados.dart
+++ lib/glados.dart
@@ -1,7 +1,7 @@
 export 'dart:math' show Random;
 
 export 'src/glados.dart';
-export 'src/any.dart' show Any, any;
+export 'src/any.dart' show Any, any, Arbitrary;
 export 'src/anys.dart';
 export 'src/utils.dart'
     show ChoosingRandomly, NoArbitraryFound, InvarianceNotDeterministic;
