a4028ca1c51a2d14adca9762290841acef8a2023
Marcel Garus <marcel.garus@gmail.com>
2020-11-02 17:27:59 +0100

    Add types in packages file

--- glados/lib/src/packages.dart
+++ glados/lib/src/packages.dart
@@ -13,7 +13,21 @@ final tuple = Package('tuple');
 
 /// This file contains info about which packages to recommend bsaed on the type
 final typeNameToPackages = <String, List<Package>>{
+  'Null': [builtIn],
+  'bool': [builtIn],
   'int': [builtIn],
+  'double': [builtIn],
+  'num': [builtIn],
+  'BigInt': [builtIn],
+  'DateTime': [builtIn],
+  'Duration': [builtIn],
   'List': [builtIn],
-  'Tuple2': [tuple, builtIn],
+  'Set': [builtIn],
+  'Map': [builtIn],
+  'Tuple2': [tuple],
+  'Tuple3': [tuple],
+  'Tuple4': [tuple],
+  'Tuple5': [tuple],
+  'Tuple6': [tuple],
+  'Tuple7': [tuple],
 };
