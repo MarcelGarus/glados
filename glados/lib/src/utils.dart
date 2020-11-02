aa8354d8f1982b5e99ddbb56c17e96b3126b3c07
Marcel Garus <marcel.garus@gmail.com>
2020-11-02 14:09:12 +0100

    Add TypeParser

--- glados/lib/src/utils.dart
+++ glados/lib/src/utils.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'structured_text.dart';
+
 /// A function with one input that's intended to be called in a test context.
 typedef Tester<T> = void Function(T input);
 
@@ -125,8 +127,70 @@ class InvarianceNotDeterministic implements Exception {
   String toString() => 'The invariance was called twice with the same value. '
       'The first time, an exception was thrown, the second time, it executed '
       'normally. The invariance has to be deterministic.';
+/// While you shouldn't rely on [Type.toString()] to return something useful, we
+/// depend on it _only_ for better developer experience.
+class RichType {
+  factory RichType.from(Type type) =>
+      _TypeParser(type.toString().replaceAll(' ', '')).parse();
+  RichType(this.name, [this.children = const []]);
+
+  final String name;
+  final List<RichType> children;
+
+  bool get hasGenerics => children.isNotEmpty;
+  Set<String> allTypes() =>
+      [name, ...children.expand((child) => child.allTypes())].toSet();
+  bool operator ==(Object other) =>
+      other is RichType &&
+      name == other.name &&
+      children.length == other.children.length &&
+      [
+        for (var i = 0; i < children.length; i++)
+          children[i] == other.children[i],
+      ].every((it) => it);
+  int get hashCode =>
+      name.hashCode +
+      children.map((child) => child.hashCode).fold(0, (a, b) => a + b);
+  String toString() => '$name<${children.join(', ')}>';
+  String toGeneratorString() {
+    final string = StringBuffer('any.${name.toLowerCamelCase()}');
+    if (children.isNotEmpty) {
+      string
+        ..write('(')
+        ..write(children.map((child) => child.toGeneratorString()).join(', '))
+        ..write(')');
+    }
+    return string.toString();
+  }
 }
 
 extension LowerCasedType on Type {
   String get lowerCamelCased => toString().toLowerCamelCase();
+class _TypeParser {
+  _TypeParser(this.string);
+
+  final String string;
+  int cursor = 0;
+
+  String get current => cursor < string.length ? string[cursor] : '';
+  void advance() => cursor++;
+
+  RichType parse() {
+    var name = StringBuffer();
+    var types = <RichType>[];
+    while (!['<', '>', ',', ''].contains(current)) {
+      name.write(current);
+      advance();
+    }
+    if (current == '>' || current == ',') {
+      return RichType(name.toString());
+    }
+    if (current == '<') {
+      while (current == '<' || current == ',') {
+        advance();
+        types.add(parse());
+      }
+    }
+    return RichType(name.toString(), types);
+  }
 }
